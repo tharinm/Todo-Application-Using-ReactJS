f71659e48ab52a07301469050be90a8dadd81b54
Tharindu Madhushan <tharindumadhushan864@gmail.com>
2023-01-06 12:41:36 +0530

    add zero length todo add issue

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-import './TodoItem.css';
-import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { useState } from "react";
+import "./TodoItem.css";
+import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
 
-
-
-function TodoItem({ todo , index ,todos ,setTodos }) {
-    
+function TodoItem({ todo, index, todos, setTodos }) {
   const [isEdit, setIsEdit] = useState(false);
   const [ediTodoName, setEditTodoName] = useState("");
 
@@ -13,66 +10,86 @@ function TodoItem({ todo , index ,todos ,setTodos }) {
     const newTodos = todos.filter((item) => {
       if (todo.id === item.id) {
         return false;
-      }
-      else {
+      } else {
         return true;
       }
     });
     setTodos(newTodos);
   };
 
-    const handleEdit = () => {
-      setIsEdit(!isEdit)
-      setEditTodoName(todo.name)
+  const handleEdit = () => {
+    setIsEdit(!isEdit);
+    setEditTodoName(todo.name);
   };
-  
+
   const handleSubmit = (e) => {
     //console.log(e);
-    if (e.keyCode === 13)
-    {
+    if (e.keyCode === 13) {
       const newTodos = todos.map((item) => {
         if (todo.id === item.id) {
           return {
             ...item,
             name: ediTodoName,
           };
-        }
-        else {
+        } else {
           return item;
         }
       });
       setTodos(newTodos);
       setIsEdit(false);
     }
-  }
+  };
 
-    return (
-      <div className="todo-item-wrapper">
-        <div className="todo-item-text">
-          <div>{index}</div>
-          {isEdit ? (
-            <input type="text" value={ediTodoName} onChange={(e) => {
-              setEditTodoName(e.target.value)
+  const handleComplete = () => {
+    const newTodos = todos.map((item) => {
+      if (todo.id === item.id) {
+        return {
+          isCompleted: !item.isCompleted,
+        };
+      } else {
+        return item;
+      }
+    });
+      setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-item-wrapper">
+      <div className="todo-item-text">
+        <div>{index}</div>
+        {isEdit ? (
+          <input
+            type="text"
+            value={ediTodoName}
+            onChange={(e) => {
+              setEditTodoName(e.target.value);
             }}
-            
-              onKeyDown={handleSubmit}
-            />
-          ) : (
-            <div>{todo.name}</div>
-          )}
-        </div>
-        <div className="todo-item-buttons">
-          {/* <div>{index}</div> */}
-
-          <button className="pencil-button" onClick={handleEdit}>
-            <PencilIcon />
-          </button>
-          <button className="trash-button" onClick={handleDelete}>
-            <TrashIcon />
-          </button>
-        </div>
+            onKeyDown={handleSubmit}
+          />
+        ) : (
+          <div
+            onClick={handleComplete}
+            style={{
+              textDecorationLine: todo.isCompleted ? "line-through" : "none",
+              textDecorationColor: "rgb(255, 90, 255)",
+            }}
+          >
+            {todo.name}
+          </div>
+        )}
       </div>
-    );
+      <div className="todo-item-buttons">
+        {/* <div>{index}</div> */}
+
+        <button className="pencil-button" onClick={handleEdit}>
+          <PencilIcon />
+        </button>
+        <button className="trash-button" onClick={handleDelete}>
+          <TrashIcon />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoItem;
